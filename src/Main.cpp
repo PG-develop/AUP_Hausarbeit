d41b0de29372f27be273139d6bb8d996a744151d
Patrick Gehrt <gehrt.develop@outlook.com>
2022-08-18 23:09:55 +0200

    Main implementiert

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,43 @@
+/* *******************************************************************************************
+* Autor:				Patrick Gehrt
+* Matr.Nr.:				214849
+* 
+* Erstelldatum:			08.08.2022
+* letzte Bearbeitung:	18.08.2022
+*
+* Fach:					Algorithmen und Programmieren
+* KursNr.:				I_ET_B_026
+* Dozent:				Prof. Dr. Fröse
+*
+* Titel:				Zahlenratespiel
+* 
+* Beschreibung:
+* In diesem Programm soll der Anwender eine zufällige generierte Zahl eraten. Die Zahl soll
+* innerhalb zweier Grenzen generiert werden.
+*
+******************************************************************************************** */
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-	
-	std::cout << "Hello World" << std::endl;
+#include "Application.hpp"
 
-	return EXIT_SUCCESS;
+namespace AUP_HA {
+
+	int main(int argc, char* argv[]) {
+	
+		try
+		{
+			// Instanz - Anwendung instanziieren und ausführen
+			std::unique_ptr<Application> app(new Application);
+			app->run();
+		}
+
+		// Wenn einer unbehandelter Fehler auftritt, wird dieser hier bearbeitet.
+		// z.B. für ein Fehlerlog
+		catch (const std::exception& e)
+		{
+			std::cout << "Es ist ein unbehandelter Fehler aufgetreten: " << e.what() << std::endl;
+		}
+
+		return EXIT_SUCCESS;
+	}
 }
